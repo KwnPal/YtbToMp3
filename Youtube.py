8694faa5efd7670ca0c4853bebba8b48bd4e65af
DounasP <ntinospalegas@gmail.com>
2024-11-19 17:39:54 +0200

    fix a minor bug

--- Youtube.py
+++ Youtube.py
@@ -6,11 +6,11 @@ from yt_dlp import DownloadError
 import sys
 import os
 
-main_path = os.path.realpath(os.path.dirname(__file__))
-main_path = os.path.dirname(main_path)
+starting_path = os.path.realpath(os.path.dirname(__file__))
+main_path = os.path.dirname(starting_path)
 path_audio = os.path.join(main_path,"Songs", "%(title)s.%(ext)s")
 path_video = os.path.join(main_path,"Videos", "%(title)s.%(ext)s")
-base_path = getattr(sys, '_MEIPASS', main_path)
+base_path = getattr(sys, '_MEIPASS', starting_path)
 
 ffmpeg_path = os.path.join(base_path, "ffmpeg", "ffmpeg.exe")
 
